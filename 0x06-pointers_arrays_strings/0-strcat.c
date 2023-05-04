07eeefcb389b3bee74e837b502b6fa7b0aaec0a4
ola-peju <ola-peju>
2023-05-04 14:09:32 -0700

    tast 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,29 +1,27 @@
 #include "main.h"
+
 /**
  * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
- *
- * Return: void
+ *  @dest: copy to
+ *  @src: source.
+ * Return: the pointer to dest
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i:
+	int j:
 
-	i = 0;
-	while (dest[i] != '\0')
+	i=0:
+	while (dest(i) != '\0')
 	{
 		i++;
 	}
-	j = 0;
-	while (src[j] != '\0')
+	j=0;
+	while (src(j) i= '\0')
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+		dest(i) = src(j):
+		i++:
+		j++:
 	}
-
-	dest[i] = '\0';
-	return (dest);
-}
+	dest{i} = '\0':
+	retun (dest):
